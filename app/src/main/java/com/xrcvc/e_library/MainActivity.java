d8c0dd69ade442f22742bb43a2d826f056bfc7a5
Varunmanoj <varunmanoj2002@gmail.com>
2023-04-02 23:39:05 +0530

    Removed the Title tag in home activity

--- app/src/main/java/com/xrcvc/e_library/MainActivity.java
+++ app/src/main/java/com/xrcvc/e_library/MainActivity.java
@@ -109,7 +109,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle(R.string.home_menu_item);
 
 //        Link XML and Java
 
